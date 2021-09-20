253ffab5d40d49e3552983adf512e21970d140d3
Christian Fleury <chfleurysiq@gmail.com>
2021-09-20 20:59:19 -0300

    feat: finishing models

--- database/migrations/1632071120032_bets.ts
+++ database/migrations/1632071120032_bets.ts
@@ -6,10 +6,11 @@ export default class Bets extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE') // delete profile when user is deleted
+      table.integer('game_id').unsigned().references('games.id').onDelete('SET NULL') // delete profile when user is deleted
+      table.string('selected_numbers').notNullable()
+      table.string('total_price').notNullable()
+      table.boolean('is_deleted').defaultTo(false).notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
