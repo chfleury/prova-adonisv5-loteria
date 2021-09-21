d4f3cede88013803136941c3374366bb770b6177
Christian Fleury <chfleurysiq@gmail.com>
2021-09-21 14:31:32 -0300

    feat: adding auth

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async login({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      const token = await auth.use('api').attempt(email, password)
+      return token
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+}
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,12 +1,28 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, beforeSave, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+import Profile from 'App/Models/Profile'
 
 export default class User extends BaseModel {
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+
+  @belongsTo(() => Profile)
+  public profile: BelongsTo<typeof Profile>
+
   @column({ isPrimary: true })
   public id: number
 
   @column()
   public email: string
+
+  @column()
+  public profileId: number
 
   @column()
   public password: string
