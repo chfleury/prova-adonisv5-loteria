d53651b6aab73f31f64c5859a66d05591717b469
Christian Fleury <chfleurysiq@gmail.com>
2021-09-24 13:59:34 -0300

    feat: adding private routes

--- app/Middleware/IsAdmin.ts
+++ app/Middleware/IsAdmin.ts
@@ -3,11 +3,15 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Profile from 'App/Models/Profile'
 
 export default class CheckProfile {
-  public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     const profileId = auth.user.$attributes.profileId
 
     const profile = await Profile.findOrFail(profileId)
 
-    if (profile.profile === 'admin') await next()
+    if (profile.profile === 'admin') {
+      await next()
+    } else {
+      response.unauthorized({ error: 'Must be an Admin' })
+    }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -10,13 +10,22 @@ Route.post('users', 'UsersController.store')
 Route.group(() => {
   Route.get('users', 'UsersController.index')
   Route.get('users/:id', 'UsersController.show')
+  Route.put('users/:id', 'UsersController.update')
+  Route.delete('users/:id', 'Userscontroller.destroy')
 
   Route.resource('bets', 'BetsController')
 
-  Route.resource('games', 'GamesController')
+  Route.get('games', 'GamesController.index')
+  Route.get('games/:id', 'GamesController.show')
 
   Route.post('forgot_password', 'ForgotPasswordController.store')
   Route.put('forgot_password', 'ForgotPasswordController.update')
-}).middleware('auth')
 
-// .middleware('isAdmin')
+  Route.group(() => {
+    Route.post('/', 'GamesController.store')
+    Route.put('/:id', 'GamesController.update')
+    Route.delete('/:id', 'GamesController.destroy')
+  })
+    .prefix('games')
+    .middleware('isAdmin')
+}).middleware('auth')
